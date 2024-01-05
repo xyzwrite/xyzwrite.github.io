4cf72f8d50a8b4358f613090e845e33dfba2cdcd
Blair Myhre <120442257+cocos-beans@users.noreply.github.com>
2024-01-05 13:14:31 -0600

    Upload dynamic index

---
+++ page/script.js
@@ -0,0 +1,18 @@
+const url_args_str = window.location.search
+const url_args = new URLSearchParams(url_args_str)
+const get_pg = url_args.get('pg')
+let pg = (get_pg !== '') ? parseInt(get_pg) : 1
+
+let page = {
+    title: document.getElementById("title"),
+    image: document.getElementById("img"),
+    body: document.getElementById("body")
+}
+
+fetch(`./assets/txt/page_${pg}.json`)
+    .then(response => { return response.json() })
+    .then(data => {
+        page.title.innerHTML = data.title
+        page.body.innerHTML = data.body
+        page.image.style.backgroundImage = `url(assets/img/${data.image.source})`
+    });
